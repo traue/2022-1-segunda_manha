55bba50bde8ba7b4df87991d15a6fddf61807eb3
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-03-14 10:21:05 -0300

    Finalização do projeto de IMC

--- CalculadoraIMC/src/java/br/uninove/pojo/Imc.java
+++ CalculadoraIMC/src/java/br/uninove/pojo/Imc.java
@@ -1,8 +1,10 @@
 package br.uninove.pojo;
 
 public class Imc {
+
     private float peso;
     private float altura;
+    private String nome;
 
     public float getPeso() {
         return peso;
@@ -19,9 +21,41 @@ public class Imc {
     public void setAltura(float altura) {
         this.altura = altura;
     }
-    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public float calculaImc() {
         float resultado = peso / (altura * altura);
         return resultado;
+    }
+
+    public String calculaClassificacao(float imc) {
+        String classificacao = "";
+
+        if (imc < 16) {
+            classificacao = "Magreza grau III";
+        } else if (imc >= 16 && imc <= 16.9) {
+            classificacao = "Magreza grau II";
+        } else if (imc >= 17 && imc <= 18.4) {
+            classificacao = "Magreza grau I";
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            classificacao = "Adequado";
+        } else if (imc >= 25 && imc <= 29.9) {
+            classificacao = "Pré-obseso";
+        } else if (imc >= 30 && imc <= 34.9) {
+            classificacao = "Obesidade grau I";
+        } else if (imc >= 35 && imc <= 39.9) {
+            classificacao = "Obesidade grau II";
+        } else {
+            classificacao = "Obseidade grau III";
+        }
+       
+        return classificacao;
     }
 }
--- CalculadoraIMC/web/calculo.jsp
+++ CalculadoraIMC/web/calculo.jsp
@@ -1,15 +1,21 @@
 <%@page import="br.uninove.pojo.Imc"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <%
-   float peso;
-   float altura;
-   Imc imc = new Imc();
-   
-   peso = Float.parseFloat(request.getParameter("peso"));
-   altura = Float.parseFloat(request.getParameter("altura"));
-   
-   //paramos aqui.. falta fazer o set dos valores, calcular e exibir
+    float peso;
+    float altura;
+    float resultado;
+    String nome;
+    Imc imc = new Imc();
 
+    peso = Float.parseFloat(request.getParameter("peso"));
+    altura = Float.parseFloat(request.getParameter("altura"));
+    nome = request.getParameter("nomeUsuario");
+
+    imc.setPeso(peso);
+    imc.setAltura(altura);
+    imc.setNome(nome);
+
+    resultado = imc.calculaImc();
 %>
 <!DOCTYPE html>
 <html>
@@ -18,5 +24,16 @@
         <title>Calculadora IMC - Resultado</title>
     </head>
     <body>
+        <h1>Calculadora IMC - Resultados</h1>
+        <hr>
+        <br>
+        <br>
+        <p>Resultados do IMC de <%= imc.getNome()%>:</p>
+        <ul>
+            <li><b>Peso informado: </b><%= String.format("%.2f", peso)%></li>
+            <li><b>Altura informada: </b><%= String.format("%.2f", altura)%></li>
+            <li><b>IMC: </b><%= String.format("%.2f", resultado)%></li>
+            <li><b>Classificação: </b><%= imc.calculaClassificacao(resultado)%></li>
+        </ul>
     </body>
 </html>
