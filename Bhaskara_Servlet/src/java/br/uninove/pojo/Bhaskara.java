c73a86b65c46e654bbdb9852f8d5d3822c98eab1
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-04-11 11:25:41 -0300

    Criação e início do projeto

---
+++ Bhaskara_Servlet/src/java/br/uninove/pojo/Bhaskara.java
@@ -0,0 +1,68 @@
+package br.uninove.pojo;
+
+public class Bhaskara {
+    
+    private int a;
+    private int b;
+    private int c;
+    private double delta;
+    private double raizPos;
+    private double raizNeg;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public double getDelta() {
+        return delta;
+    }
+
+    public void setDelta(double delta) {
+        this.delta = delta;
+    }
+
+    public double getRaizPos() {
+        return raizPos;
+    }
+
+    public void setRaizPos(double raizPos) {
+        this.raizPos = raizPos;
+    }
+
+    public double getRaizNeg() {
+        return raizNeg;
+    }
+
+    public void setRaizNeg(double raizNeg) {
+        this.raizNeg = raizNeg;
+    }
+    
+    public void calculaBhaskara() {
+        setDelta((getB() * getB()) - 4 * getA() * getC());
+        if(delta > 0) {
+            setRaizPos((-getB() + Math.sqrt(getDelta())) / (2 * getA()));
+            setRaizNeg((-getB() - Math.sqrt(getDelta())) / (2 * getA()));
+        }
+    }
+
+}
